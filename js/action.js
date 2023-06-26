781bb2a6fce0203726bfbb522a073cf8ddba94ae
benf-le <banglebang2016@gmail.com>
2023-06-27 03:08:37 +0700

    update profile

--- js/action.js
+++ js/action.js
@@ -3,6 +3,9 @@ var matchesContent = document.getElementById("matchesContent");
 var chatButton = document.getElementById("chatButton");
 var matchesButton = document.getElementById("matchesButton");
 
+var chatDisplay = document.getElementById("chatDisplay");
+var swiperDisplay = document.getElementById("swiperDisplay");
+
 function toggleChat() {
     chatContent.style.display = "none";
     matchesContent.style.display = "block";
@@ -15,4 +18,17 @@ function toggleMatches() {
     chatContent.style.display = "block";
     chatButton.style.borderBottom = "solid 4px rgb(243, 33, 33)";
     document.getElementById("matchesButton").style.borderBottom = "none";
+}
+
+function mobileChat() {
+    swiperDisplay.style.display = "none";
+    chatDisplay.style.display = "block";
+    chatDisplay.style.width = "100%";
+    chatDisplay.style.height = "100%";
+
+}
+
+function mobileMatches() {
+    chatDisplay.style.display = "none";
+    swiperDisplay.style.display = "flex";
 }
