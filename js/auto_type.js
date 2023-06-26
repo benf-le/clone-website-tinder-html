989ac846937493ecce73eb4140a1431c4a94ed91
benf-le <banglebang2016@gmail.com>
2023-06-27 05:13:08 +0700

    big update

--- js/auto_type.js
+++ js/auto_type.js
@@ -1,9 +1,8 @@
-
 var options = {
-  strings: ["Love at first chat"],
-  typeSpeed: 70,
-  backSpeed: 30,
-  loop: true,
+    strings: ["Love at first chat"],
+    typeSpeed: 70,
+    backSpeed: 30,
+    loop: true,
 };
 
 var typed = new Typed(".slogan-app", options); 
