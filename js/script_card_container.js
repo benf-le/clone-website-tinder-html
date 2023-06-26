69a6556ff88cde9dd9e69734f614809d4335eb0e
benf-le <banglebang2016@gmail.com>
2023-06-27 05:13:23 +0700

    big update

--- js/script_card_container.js
+++ js/script_card_container.js
@@ -5,12 +5,12 @@ const dislike = document.querySelector('#dislike');
 
 // constants
 const urls = [
-  'https://i.imgur.com/oPj4A8u.jpg',
-  'https://i.imgur.com/OckVkRo.jpg',
-  'https://i.imgur.com/Al4nSX8.jpg',
-  'https://i.imgur.com/Q9WPlWA.jpg',
-  'https://i.imgur.com/MWAcQRM.jpg',
-  'https://i.imgur.com/H07Fxdh.jpg'
+    'https://i.imgur.com/oPj4A8u.jpg',
+    'https://i.imgur.com/OckVkRo.jpg',
+    'https://i.imgur.com/Al4nSX8.jpg',
+    'https://i.imgur.com/Q9WPlWA.jpg',
+    'https://i.imgur.com/MWAcQRM.jpg',
+    'https://i.imgur.com/H07Fxdh.jpg'
 ];
 
 // variables
@@ -18,29 +18,29 @@ let cardCount = 0;
 
 // functions
 function appendNewCard() {
-  const card = new Card({
-    imageUrl: urls[cardCount % 5],
-    onDismiss: appendNewCard,
-    onLike: () => {
-      like.style.animationPlayState = 'running';
-      like.classList.toggle('trigger');
-    },
-    onDislike: () => {
-      dislike.style.animationPlayState = 'running';
-      dislike.classList.toggle('trigger');
-    }
-  });
-  swiper.append(card.element);
-  cardCount++;
+    const card = new Card({
+        imageUrl: urls[cardCount % 5],
+        onDismiss: appendNewCard,
+        onLike: () => {
+            like.style.animationPlayState = 'running';
+            like.classList.toggle('trigger');
+        },
+        onDislike: () => {
+            dislike.style.animationPlayState = 'running';
+            dislike.classList.toggle('trigger');
+        }
+    });
+    swiper.append(card.element);
+    cardCount++;
 
-  const cards = swiper.querySelectorAll('.card:not(.dismissing)');
-  cards.forEach((card, index) => {
-    card.style.setProperty('--i', index);
-  });
+    const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+    cards.forEach((card, index) => {
+        card.style.setProperty('--i', index);
+    });
 }
 
 // first 5 cards
 for (let i = 0; i < 5; i++) {
-  appendNewCard();
+    appendNewCard();
 }
 
